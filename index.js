7e15837eb30c75c2686ce89a2d5bad26dfad0ef7
Inna <inndi.spivakova@gmail.com>
2021-12-20 18:08:57 +0200

    feat: create overlay; create html and css for popup box

---
+++ index.js
@@ -0,0 +1,12 @@
+let profile = document.querySelector(".profile");
+let editButton = profile.querySelector('.profile__edit-btn');
+// console.log(editButton);
+
+function renderPopup() {
+  profile.innerHTML = `
+  div.profile
+  `
+
+}
+
+editButton.addEventListener("click", renderPopup);
