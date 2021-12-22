23916a9653aa1387fe83af16e5d861030d4fad6e
Inna <inndi.spivakova@gmail.com>
2021-12-22 20:52:19 +0200

    fix: replace image; replace popup block;

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,12 @@
-const profile = document.querySelector(".profile");
+const body = document.querySelector(".page");
+const profile = body.querySelector(".profile");
 const profileName = profile.querySelector(".profile__name");
 const profileInfo = profile.querySelector(".profile__about-me");
 const editButton = profile.querySelector(".profile__edit-btn");
-const popupBox = profile.querySelector(".popup");
+const popupBox = body.querySelector(".popup");
 const popupForm = popupBox.querySelector(".popup__form");
-const fieldName = popupBox.querySelector(".popup__name");
-const fieldAboutMe = popupBox.querySelector(".popup__about-me");
+const fieldName = popupBox.querySelector(".popup__input_field_name");
+const fieldAboutMe = popupBox.querySelector(".popup__input_field_about-me");
 const saveButton = popupBox.querySelector(".popup__save-btn");
 const closeButton = popupBox.querySelector(".popup__close-btn");
 
